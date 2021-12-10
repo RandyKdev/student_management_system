45ede3d7d02613e5d4294c391397bd6befa8ff3f
Randy Kwalar <rkwalar@gmail.com>
2021-12-10 14:21:43 +0100

    Add login and error screen

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,8 +11,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-			Scene scene = new Scene(root,600,400);
+			// Parent root = FXMLLoader.load(getClass().getResource("login.fxml")); // for login
+			// Scene scene = new Scene(root,600,400); // for login
+			Parent root = FXMLLoader.load(getClass().getResource("error.fxml")); // error
+			Scene scene = new Scene(root,640,285); // for error
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
