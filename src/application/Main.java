88e69fbf6c390f42689ceb2651479cf1a9cca309
Tabot Chares Bessong <ebezebeatrice@gmail.com>
2021-12-10 15:15:41 +0100

    started with and will finish registration

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("registration.fxml"));
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
