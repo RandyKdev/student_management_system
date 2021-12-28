828525d72c189e633d15bd23314d10d5c2a0c608
Randy Kwalar <rkwalar@gmail.com>
2021-12-28 20:29:03 +0100

    Add admin user methods

--- src/database/adminDB.java
+++ src/database/adminDB.java
@@ -5,12 +5,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserDB {
+public class adminDB {
 	public boolean onLogin(String email, String pwd) {
 		DBConnection con = new DBConnection();
 		Connection connection = con.getDbConnection();
 		try {
-			String sql = "SELECT email, password FROM user "
+			String sql = "SELECT email, password FROM admin "
 				     + "WHERE email = ? AND password = ?;";
 			PreparedStatement statement = connection.prepareStatement(sql);
 			statement.setString(1, email);
@@ -35,15 +35,20 @@ public class UserDB {
 		DBConnection con = new DBConnection();
 		Connection connection = con.getDbConnection();
 		try {
-			String sql =  "SELECT email FROM user "
+			String sql =  "SELECT email FROM admin "
 				     + "WHERE email = ?;";
 			PreparedStatement statement = connection.prepareStatement(sql);
 			statement.setString(1, email);
 			
-			statement.executeQuery();
+			ResultSet result = statement.executeQuery();
+			
+			if(result.next()) {
+				connection.close();
+				return true;
+			}
 			
 			connection.close();
-			return true;
+			return false;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -55,7 +60,7 @@ public class UserDB {
 		DBConnection con = new DBConnection();
 		Connection connection = con.getDbConnection();
 		try {
-			String sql = "INSERT INTO user"
+			String sql = "INSERT INTO admin"
 					+ "(email, password)"
 					+ "VALUES"
 					+ "(?, ?);";
